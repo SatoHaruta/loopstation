13cff97bb292eda68ce06bfd9769ce0cc23d855b
SatoHaruta <poruting@gmail.com>
2023-08-05 19:13:01 +0900

    setDurationStateとgetDurationOverで一定期間に一フレームのみstateを変えられるようになった

--- DurationManager.js
+++ DurationManager.js
@@ -1,6 +1,7 @@
 //ここでは、最初の録音のタイミングを取得し、そのdurationを定義している
 class DurationManager{
 
+durationState = "waiting";
 globalCurrentTime = 0;//現在の時間
 globalTimer;//最初に録音されてから、時間を測り続けるタイマー
 globalDuration = 0;//再生の基準値
@@ -12,6 +13,8 @@ constructor(){
 
 DurationManagerUpdate(){
     this.getFirstRecord();
+    this.setDurationState();
+    console.log(this.durationState + " / " + this.globalTimer.getElapsedTime() + " / " + this.globalDuration);
     //最初の録音が登録された瞬間に
     if(this.globalIsSetState == "recordingSet"){
         this.setGlobalDuration();
@@ -70,6 +73,27 @@ setGlobalDuration() {
             this.globalDuration = trackManager[i].soundManager[0].duration;
             if(developerMode){console.log("globalDuration設定した")};
         }
+    }
+}
+
+//常時globalTimerを監視し、globalDurationを超えた瞬間にStateを切り替える
+setDurationState(){
+    if(this.durationState == "waiting" && this.getDurationOver() == true){
+        this.durationState = "durationOver";
+        this.resetGlobalTimer();
+    }
+    else if(this.durationState == "durationOver"){
+        this.durationState = "waiting";
+    }
+}
+
+//現在の時間がglobalDurationを超えた時のみtrueを返す
+getDurationOver(){
+    if(this.globalTimer.getElapsedTime() > this.globalDuration){
+        return true;
+    }
+    else{
+        return false;
     }
 }
 
